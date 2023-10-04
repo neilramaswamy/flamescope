36560c63f42437ea8292836c671fac44a5562c8d
Neil Ramaswamy <neil.ramaswamy@databricks.com>
2023-10-04 10:52:47 -0700

    Always use 127.0.0.1:5000

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from app import APP
 
 def main(argv):
     """Start development web server."""
-    APP.run(host=APP.config['HOST'], port=APP.config['PORT'], threaded=True)
+    APP.run(host="127.0.0.1", port=5000, threaded=True)
 
 if __name__ == "__main__":
     main(sys.argv)
